67e75a9dbf4163b50f56fed7133e323ece6b5ad0
Murph Grainger <murph.grainger@gmail.com>
2016-12-21 09:44:32 -0700

    validated id route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var queries = require("../queries");
+const express = require('express');
+const router = express.Router();
+const queries = require("../queries");
 const knex = require('../db/knex');
-
+const Joi = require('joi');
 
 router.get("/", function(request, response, next) {
-    response.render("home");
+    response.redirect('/vineyard');
 });
 
 router.get("/vineyard", function(request, response, next) {
@@ -21,18 +21,22 @@ router.get("/add", function(request, response, next) {
 });
 
 router.post('/vineyard', function(req, res, next) {
-    knex('vineyard').insert({
-            name: req.body.name,
-            address: req.body.address,
-            phone: req.body.phone,
-            website: req.body.website,
-            appointment: req.body.appointment,
-            wine_purchase: req.body.wine_purchase,
-            visit_date: req.body.visit_date
-        }, 'id')
-        .then(function(vineyard) {
-            res.redirect('/vineyard/' + vineyard);
-        });
+    if (validVineyard(req.body)) {
+        knex('vineyard').insert({
+                name: req.body.name,
+                address: req.body.address,
+                phone: req.body.phone,
+                website: req.body.website,
+                appointment: req.body.appointment,
+                wine_purchase: req.body.wine_purchase,
+                visit_date: req.body.visit_date
+            }, 'id')
+            .then(function(vineyard) {
+                res.redirect('/vineyard/' + vineyard);
+            });
+    } else {
+        resError(res, 500, 'Not a valid entry!');
+    }
 });
 
 router.get("/vineyard/:id/edit", function(req, res, next) {
@@ -43,9 +47,7 @@ router.get("/vineyard/:id/edit", function(req, res, next) {
     });
 });
 
-
 router.put('/vineyard/:id', function(req, res, next) {
-    console.log(req.body);
     knex('vineyard').where('id', req.params.id).update(req.body)
         .then(function() {
             res.redirect('/vineyard/' + req.params.id);
@@ -59,13 +61,33 @@ router.delete('/vineyard/:id', function(req, res, next) {
 });
 
 router.get("/vineyard/:id", function(req, res, next) {
-    knex('vineyard').where('id', req.params.id).first()
-        .then(function(data) {
-            res.render('id', {
-                vineyard: data
+    if (!isNaN(req.params.id)) {
+        knex('vineyard').where('id', req.params.id).first()
+            .then(function(vineyard) {
+                if (vineyard) {
+                    res.render('id', {
+                        vineyard: vineyard
+                    });
+                } else {
+                    resError(res, 404, 'Vineyard does not exist!');
+                }
             });
-        });
+    } else {
+        resError(res, 500, 'Use a valid ID');
+    }
 });
 
+function validVineyard(vineyard) {
+    return typeof vineyard.name == 'string' &&
+        vineyard.name.trim() !== '' &&
+        typeof vineyard.address == 'string' &&
+        typeof vineyard.phone == 'string' &&
+        typeof vineyard.website == 'string';
+}
+
+function resError(res, statusCode, message) {
+    res.status(statusCode);
+    res.send(message);
+}
 
 module.exports = router;
